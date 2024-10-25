99e21fe0c2f6808a8d22c1b3d99eb53e30a39ea2
grez-lucas <lucasgrez3010@gmail.com>
2024-10-25 19:59:57 -0300

    feat(auth): Add initial auth middleware

---
+++ auth.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func WriteUnauthorized(w http.ResponseWriter) {
+
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+
+}
+
+// To decorate certain HTTP handlers with JWT authentication (the ones who
+// require it)
+
+func WithJWTAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+
+	log.Println("Calling JWT middleware")
+
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		tokenString := req.Header.Get("x-jwt-token")
+
+		if _, err := ValidateJWT(tokenString); err != nil {
+			WriteJSON(w, http.StatusForbidden, APIError{"Invalid token"})
+			return
+		}
+
+		handlerFunc(w, req)
+	}
+}
+
+func ValidateJWT(tokenString string) (*jwt.Token, error) {
+
+	jwtSecret, found := os.LookupEnv("JWT_SECRET")
+
+	if !found {
+		return nil, fmt.Errorf("Couldn't find environment variable JWT_SECRET")
+	}
+
+	return jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: `%v`", t.Header["&alg"])
+		}
+		return []byte(jwtSecret), nil
+	})
+
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/grez-lucas/go-gym
 go 1.23.2
 
 require github.com/lib/pq v1.10.9
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
