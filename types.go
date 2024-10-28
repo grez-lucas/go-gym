d908b9f19e4cd8ede61b251703c064ed085ea205
grez-lucas <lucasgrez3010@gmail.com>
2024-10-28 15:48:14 -0300

    feat(types) Add account type

--- types.go
+++ types.go
@@ -54,3 +54,25 @@ func NewRating(gymID int, rating int, userName string, review string) *Rating {
 		UpdatedAt: time.Now().UTC(),
 	}
 }
+
+type CreateAccountRequest struct {
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+type Account struct {
+	ID        int       `json:"id"`
+	UserName  string    `json:"userName"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func NewAccount(userName string, password string) *Account {
+	return &Account{
+		UserName:  userName,
+		Password:  password,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	}
+}
